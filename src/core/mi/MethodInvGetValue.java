5071116338dfcb036d9a63e445392d2f78c8763e
Chouaib <hentablichouaib@yahoo.fr>
2017-02-20 10:43:05 +0100

    edit

---
+++ src/core/mi/MethodInvGetValue.java
@@ -0,0 +1,13 @@
+package core.mi;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Created by chouaib on 20/02/17.
+ */
+public class MethodInvGetValue implements Callable {
+    @Override
+    public Object call() throws Exception {
+        return null;
+    }
+}
